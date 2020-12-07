efe4e748ac4a2395aa915fbb38a22fed46719c41
Manish Kumar Rajak <52307777+visionxStudio@users.noreply.github.com>
2020-12-07 12:05:34 +0545

    first commit

---
+++ lib/other.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Other extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("other Page"),
+      ),
+      body: Container(),
+    );
+  }
+}
